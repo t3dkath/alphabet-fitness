09d57dd8e41a8bb37fc7a694a48cc5dc4f1fc19f
Kath Faulkner <kath@flipsidegroup.com>
2015-12-28 22:10:22 +0000

    error message when nothing added. disable all buttons when showing error

--- alphabet-fitness/HomeVC.swift
+++ alphabet-fitness/HomeVC.swift
@@ -18,6 +18,10 @@ class HomeVC: UIViewController {
     @IBOutlet weak var errorBgImg: UIImageView!
     @IBOutlet weak var errorCloseBtn: UIButton!
     
+    @IBOutlet weak var wotdBtn: UIButton!
+    @IBOutlet weak var inputBtn: UIButton!
+    @IBOutlet weak var exercisesBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,6 +35,11 @@ class HomeVC: UIViewController {
         errorLbl.hidden = true
         errorBgImg.hidden = true
         errorCloseBtn.hidden = true
+        
+        wordInput.enabled = true
+        wotdBtn.enabled = true
+        inputBtn.enabled = true
+        exercisesBtn.enabled = true
     }
     @IBAction func showAllExercises() {
         performSegueWithIdentifier("AlphaSegue", sender: self)
@@ -41,7 +50,7 @@ class HomeVC: UIViewController {
             ExerciseManager.instance.setWorkoutForWord(word)
             performSegueWithIdentifier("WordSegue", sender: self)
         } else {
-            showError("Not a word")
+            showError("Hmm, we didn't detect a character string with any recognisable letters, please try again")
         }
     }
 
@@ -56,6 +65,11 @@ class HomeVC: UIViewController {
         errorLbl.hidden = false
         errorBgImg.hidden = false
         errorCloseBtn.hidden = false
+        
+        wordInput.enabled = false
+        wotdBtn.enabled = false
+        inputBtn.enabled = false
+        exercisesBtn.enabled = false
     }
 
 }
